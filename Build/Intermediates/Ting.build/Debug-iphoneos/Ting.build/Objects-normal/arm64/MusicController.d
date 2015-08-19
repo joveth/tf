2a4fd4283c131ab6bb126532312e3d57f8864915
qiaowei <jov123@163.com>
2015-08-19 14:34:38 +0800

    icons

--- Build/Intermediates/Ting.build/Debug-iphoneos/Ting.build/Objects-normal/arm64/MusicController.d
+++ Build/Intermediates/Ting.build/Debug-iphoneos/Ting.build/Objects-normal/arm64/MusicController.d
@@ -1,2 +1,2 @@
-dependencies: /Users/shuwei/works/Ting/MusicController.m \
-  /Users/shuwei/works/Ting/MusicController.h
+dependencies: /Users/shuwei/works/Ting/Ting/MusicController.m \
+  /Users/shuwei/works/Ting/Ting/MusicController.h
